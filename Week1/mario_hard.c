a46c8332893c2730ee03da0ea3c4252668537440
Chetan Kini <ckini123@gmail.com>
2019-12-13 11:16:51 -0500

    Update mario_hard.c

--- Week1/mario_hard.c
+++ Week1/mario_hard.c
@@ -1,1 +1,31 @@
+#include <stdio.h>
 
+void repeat(char input, int count);
+
+int main() {
+  int height;
+  do {
+  printf("Height: ");
+  scanf("%d", &height);
+  } while (height < 2 || height > 8);
+
+  int spaces = height - 1;
+  int hashes = 1;
+
+  for (int i = 0; i < height; i++) {
+    repeat(' ', spaces);
+    repeat('#', hashes);
+    printf(" ");
+    repeat('#', hashes);
+    repeat(' ', spaces);
+    printf("\n");
+    hashes++;
+    spaces--;
+  }
+}
+
+void repeat(char input, int count) {
+  for (int i = 0; i < count; i++) {
+    printf("%c", input);
+  }
+}
