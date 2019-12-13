e49753b095d059d44c9907ce2b77d057f9a85b56
Chetan Kini <ckini123@gmail.com>
2019-12-13 11:13:34 -0500

    Update mario_easy.c

--- Week1/mario_easy.c
+++ Week1/mario_easy.c
@@ -1,1 +1,28 @@
+#include <stdio.h>
 
+void repeat(char input, int count);
+
+int main() {
+  int height;
+  do {
+  printf("Height: ");
+  scanf("%d", &height);
+  } while (height < 2 || height > 8);
+
+  int spaces = height - 1;
+  int hashes = 1;
+
+  for (int i = 0; i < height; i++) {
+    repeat(' ', spaces);
+    repeat('#', hashes);
+    printf("\n");
+    hashes++;
+    spaces--;
+  }
+}
+
+void repeat(char input, int count) {
+  for (int i = 0; i < count; i++) {
+    printf("%c", input);
+  }
+}
