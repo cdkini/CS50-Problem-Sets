eff8341afbdce7f69aabcca82cce7a6d5167d4a2
Chetan Kini <ckini123@gmail.com>
2019-12-13 13:03:09 -0500

    Update credit.c

--- Week1/credit.c
+++ Week1/credit.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
 
+int countDigits(int num);
+
+int main() {
+    unsigned long cardnum = 0;
+    do {
+        printf("Number: ");
+        scanf("%lu", &cardnum);
+    } while (countDigits(cardnum) < 13 || countDigits(cardnum) >16);
+    
+    return 0;
+}
+
+int countDigits(int num) {
+    int count = 0;
+    while (num != 0) {
+        num /= 10;
+        count++;
+    }
+    return count;
+}
