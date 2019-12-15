17780f144e0830b734c56307373366062b959f8d
Chetan Kini <ckini123@gmail.com>
2019-12-14 23:25:37 -0500

    Update readability.c

--- Week2/readability.c
+++ Week2/readability.c
@@ -3,48 +3,61 @@
 #include <math.h>
 #include <ctype.h>
 
-int iterate_text(string text, char type);
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
 int coleman_liau(float l, float s);
 
 
 int main(void) {
     string text = get_string("Text: ");
-    printf("Letters: %d\n", iterate_text(text, 'l'));
-    printf("Words: %d\n", iterate_text(text, 'w'));
-    printf("Sentences: %d\n", iterate_text(text, 's'));
-    int cl_index = coleman_liau(1.0, 2.0);
-    printf("%d\n", cl_index);
-    return 0;
+
+    float l = (float) count_letters(text) / count_words(text) * 100;
+    float s = (float) count_sentences(text) / count_words(text) * 100;
+    int cl_index = coleman_liau(l, s);
+
+    if (cl_index >= 1 && cl_index <= 16) {
+        printf("Grade %d\n", cl_index);
+    }
+    else if (cl_index > 16) {
+        printf("Grade 16+\n");
+    }
+    else {
+        printf("Before Grade 1\n");
+    }
 }
 
-
-int iterate_text(string text, char type) {
-    int lws_counts[] = {0, 1, 0};
+int count_letters(string text) {
+    int count = 0;
     for (int i = 0; text[i] != '\0'; i++) {
         if (isalpha(text[i])) {
-            lws_counts[0]++;
-        }
-        else if (text[i] == ' ') {
-            lws_counts[1]++;
-        }
-        else if (text[i] == '.' || text[i] == '!' || text[i] == '?')  {
-            lws_counts[2]++;
+            count++;
         }
     }
-    if (type == 'l') {
-        return lws_counts[0];
-    }
-    else if (type == 'w') {
-        return lws_counts[1];
-    }
-    else if (type == 's') {
-        return lws_counts[2];
-    }
-    return 0;
+    return count;
 }
 
+int count_words(string text) {
+    int count = 1;
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (text[i] == ' ') {
+            count++;
+        }
+    }
+    return count;
+}
+
+int count_sentences(string text) {
+    int count = 0;
+    for (int i = 0; text[i] != '\0'; i++) {
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?') {
+            count++;
+        }
+    }
+    return count;
+}
 
 int coleman_liau(float l, float s) {
     float index = 0.0588 * l - 0.296 * s - 15.8;
-    return (int) round(index);
+    return round(index);
 }
