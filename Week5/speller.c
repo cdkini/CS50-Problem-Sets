b56ac00ef9d7f1f5bada0dab14265db0a4d1254f
Chetan Kini <ckini123@gmail.com>
2020-02-23 21:02:13 -0500

    Update speller.c

--- Week5/speller.c
+++ Week5/speller.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <strings.h>
 
 #include "dictionary.h"
 
@@ -17,29 +18,27 @@ node;
 // Number of buckets in hash table
 const unsigned int N = 26;
 
+// Number of total words
+unsigned int wordCount;
+
 // Hash table
 node *table[N];
 
 // Returns true if word is in dictionary else false
 bool check(const char *word) {
-
-    char *newWord = malloc(strlen(word));
-    for (int i = 0; word[i]; i++) {
-        newWord[i] = tolower(word[i]);
+    char firstChar;
+    if (word[0] >= 97) {
+        firstChar = word[0];
+    } else {
+        firstChar = word[0] + 32;
     }
 
-    unsigned int hashVal = hash(newWord);
+    unsigned int index = hash(&firstChar);
 
-    if (table[hashVal] == NULL) {
-        free(newWord);
-        return false;
-    }
-
-    node **head = &table[hashVal];
-    node *trav = *head;
-    while (trav->next != NULL) {
-        if (strcmp(trav->word, newWord) == 0) {
-            free(newWord);
+    node *head = table[index];
+    node *trav = head;
+    while (trav != NULL) {
+        if (strcasecmp(trav->word, word) == 0) {
             return true;
         }
         trav = trav->next;
@@ -57,7 +56,6 @@ unsigned int hash(const char *word) {
 bool load(const char *dictionary) {
 
     FILE *f = fopen(dictionary, "r");
-
     if (f == NULL) {
         return false;
     }
@@ -71,45 +69,33 @@ bool load(const char *dictionary) {
 
         strcpy(n->word, word); // Set the new node's word attribute to the word we've read from the file
         unsigned int index = hash(word); // Utilize the hash function to determine the proper hash table index to insert the word into
-        if (table[index] == NULL) {
-            table[index] = n;
-            free(word);
-            return true;
-        }
-        node **head = &table[index];
-        n->next = *head;
-        head = &n;
+        node *head = table[index];
+        n->next = head;
+        head = n;
+        wordCount++;
+        free(n);
     }
-
     free(word);
+    fclose(f);
     return true;
 }
 
 // Returns number of words in dictionary if loaded else 0 if not yet loaded
 unsigned int size(void) {
-    unsigned int count = 0;
-
-    for (int i = 0; i < N; i++) {
-        node **head = &table[i];
-        node *trav = *head;
-        while (trav->next != NULL) {
-            trav = trav->next;
-            count++;
-        }
-    }
-    return count;
+    return wordCount;
 }
 
 // Unloads dictionary from memory, returning true if successful else false
 bool unload(void) {
     for (int i = 0; i < N; i++) {
-        // Traverse linked lists within hash table to free any malloced pointers
-        node **head = &table[i];
-        node *trav = *head;
-        while (trav->next != NULL) {
+        node *head = table[i];
+        node *trav = head;
 
+        while (trav != NULL) {
+            node *tmp = trav;
             trav = trav->next;
+            free(tmp);
         }
     }
-    return false;
+    return true;
 }
