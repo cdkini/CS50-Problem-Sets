cbcf71c868236bf55b73ece2c2289d43fb6fad7e
Chetan Kini <ckini123@gmail.com>
2020-03-01 18:37:19 -0500

    Update cash.py

--- Week6/cash.py
+++ Week6/cash.py
@@ -1,1 +1,13 @@
+def main():
+  change = float(input("Change owed: "))
+  count = 0
+  coins = [0.25, 0.1, 0.05, 0.01]
+  
+  for coin in coins:
+    change -= change // coin * coin
+    count += change // coin
+    
+  print(int(count))
 
+if __name__ == "__main__":
+  main()
