56bf4d0a0d3783121c7bd569414198c7a204ad8b
Chetan Kini <ckini123@gmail.com>
2020-03-01 18:20:23 -0500

    Update mario_hard.py

--- Week6/mario_hard.py
+++ Week6/mario_hard.py
@@ -1,1 +1,9 @@
+def main():
+  height = int(input("Height: "))
+  for i in range(height):
+    a = (' ' * (height-i+1)) + ('#' * (i+1))
+    print(a + ' ' + a[::-1])
 
+
+if __name__ == "__main__":
+  main()
