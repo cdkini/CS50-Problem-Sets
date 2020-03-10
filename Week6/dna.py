c71986ec3db3726c02241c28223a15d26e8a86e3
Chetan Kini <ckini123@gmail.com>
2020-03-09 21:57:33 -0400

    Update dna.py

--- Week6/dna.py
+++ Week6/dna.py
@@ -3,50 +3,57 @@ import sys
 
 
 def generate_str_list(reader):
+    '''Determines list of STR's to check for'''
     str_list = []
     for i, row in enumerate(reader):
         if not i:
             str_list += row[1:]
             break
+        
     return str_list
 
 
 def generate_str_dict(reader):
+    '''Creates dictionary pairing STR counts and names'''
     str_dict = {}
     for i, row in enumerate(reader):
         if i:
             str_dict[tuple(int(num) for num in row[1:])] = row[0] 
+
     return str_dict
 
 
-def count_str(seq, dna):
+def count_consecutive_str(seq, dna):
+    '''Counts back-to-back STR's'''
     seq_len = len(seq)
-    found = False
+    i = 0
+    while dna[i:i+seq_len] != seq:
+        i += 1
+    buffer = dna[i:]
+
+    multiple = 1
     count = 0
-    for i in range(len(dna)):
-        if not found:
-            if dna[i:i+seq_len] == seq:
-                found = True
-        if found:
-            for j in range(i, len(dna)-i, seq_len):
-                if dna[j:j+seq_len] == seq:
-                    count += 1
-                else:
-                    return count
+    while seq * multiple == buffer[:multiple*seq_len]:
+        multiple += 1
+        count += 1
+
     return count
-        
+
     
 def main():
+
     if len(sys.argv) != 3:
         print("Error! Please ensure that you've passed two args!")
         sys.exit(1)
+
     with open(sys.argv[1], 'r') as db_file:
         reader = csv.reader(db_file)
         str_list = generate_str_list(reader) 
         str_dict = generate_str_dict(reader)
+
     with open(sys.argv[2], 'r') as dna_file:
         dna = dna_file.read()
-        target = tuple([count_str(seq, dna) for seq in str_list])
+        target = tuple([count_consecutive_str(seq, dna) for seq in str_list])
         print(str_dict.get(target, "No match"))
         
 
