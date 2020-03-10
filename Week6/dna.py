0a7e0185b3c7c7496fd711359f39da39add3c994
Chetan Kini <ckini123@gmail.com>
2020-03-09 20:51:26 -0400

    Update dna.py

--- Week6/dna.py
+++ Week6/dna.py
@@ -2,42 +2,53 @@ import csv
 import sys
 
 
-def generate_str_dict(file):
-    reader = csv.reader(file)
-    str_dict = {}
+def generate_str_list(reader):
+    str_list = []
     for i, row in enumerate(reader):
         if not i:
-            str_dict[row[0]] = tuple(val for val in row[1:])
-        else:
-            str_dict[tuple(int(val) for val in row[1:])] = row[0]
-    print(str_dict)
+            str_list += row[1:]
+            break
+    return str_list
+
+
+def generate_str_dict(reader):
+    str_dict = {}
+    for i, row in enumerate(reader):
+        if i:
+            str_dict[tuple(int(num) for num in row[1:])] = row[0] 
     return str_dict
 
 
-def longest_repeating_str(target_str, sequence):
-    count = 1
-    while True:
-        if target_str * count in sequence:
-            count += 1
-        else:
-            return count
-
-
+def count_str(seq, dna):
+    seq_len = len(seq)
+    found = False
+    count = 0
+    for i in range(len(dna)):
+        if not found:
+            if dna[i:i+seq_len] == seq:
+                found = True
+        if found:
+            for j in range(i, len(dna)-i, seq_len):
+                if dna[j:j+seq_len] == seq:
+                    count += 1
+                else:
+                    return count
+    return count
+        
+    
 def main():
     if len(sys.argv) != 3:
-        print("Error! Please ensure that you've passed two args")
+        print("Error! Please ensure that you've passed two args!")
         sys.exit(1)
     with open(sys.argv[1], 'r') as db_file:
-        str_dict = generate_str_dict(db_file)
-        with open(sys.argv[2], 'r') as sequence_file:
-            sequence = sequence_file.read()
-            check = []
-            for dna_str in str_dict["name"]:
-                check.append(longest_repeating_str(dna_str, sequence))
-            print(check[::-1])
-            result = str_dict.get(tuple(str(val) for val in check), "No match")
-            print(result)
-
+        reader = csv.reader(db_file)
+        str_list = generate_str_list(reader) 
+        str_dict = generate_str_dict(reader)
+    with open(sys.argv[2], 'r') as dna_file:
+        dna = dna_file.read()
+        target = tuple([count_str(seq, dna) for seq in str_list])
+        print(str_dict.get(target, "No match"))
+        
 
 if __name__ == "__main__":
     main()
